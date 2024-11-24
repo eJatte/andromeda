b5ffeb09029f03f63ce4aa9a9475460956ad54c4
ejatte <elias.jatte@gmail.com>
2024-11-24 17:34:51 +0100

    basic triangle!

--- src/main/java/andromeda/HelloWorld.java
+++ src/main/java/andromeda/HelloWorld.java
@@ -5,11 +5,12 @@ import org.lwjgl.glfw.*;
 import org.lwjgl.opengl.*;
 import org.lwjgl.system.*;
 
+import java.io.*;
 import java.nio.*;
 
 import static org.lwjgl.glfw.Callbacks.*;
 import static org.lwjgl.glfw.GLFW.*;
-import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.opengl.GL46C.*;
 import static org.lwjgl.system.MemoryStack.*;
 import static org.lwjgl.system.MemoryUtil.*;
 
@@ -39,7 +40,7 @@ public class HelloWorld {
         GLFWErrorCallback.createPrint(System.err).set();
 
         // Initialize GLFW. Most GLFW functions will not work before doing this.
-        if ( !glfwInit() )
+        if (!glfwInit())
             throw new IllegalStateException("Unable to initialize GLFW");
 
         // Configure GLFW
@@ -51,18 +52,18 @@ public class HelloWorld {
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
         // Create the window
-        window = glfwCreateWindow(300, 300, "Hello World!", NULL, NULL);
-        if ( window == NULL )
+        window = glfwCreateWindow(1280, 720, "Hello World!", NULL, NULL);
+        if (window == NULL)
             throw new RuntimeException("Failed to create the GLFW window");
 
         // Setup a key callback. It will be called every time a key is pressed, repeated or released.
         glfwSetKeyCallback(window, (window, key, scancode, action, mods) -> {
-            if ( key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE )
+            if (key == GLFW_KEY_ESCAPE && action == GLFW_RELEASE)
                 glfwSetWindowShouldClose(window, true); // We will detect this in the rendering loop
         });
 
         // Get the thread stack and push a new frame
-        try ( MemoryStack stack = stackPush() ) {
+        try (MemoryStack stack = stackPush()) {
             IntBuffer pWidth = stack.mallocInt(1); // int*
             IntBuffer pHeight = stack.mallocInt(1); // int*
 
@@ -98,20 +99,96 @@ public class HelloWorld {
         GL.createCapabilities();
 
         // Set the clear color
-        glClearColor(1.0f, 0.0f, 0.0f, 0.0f);
+        glClearColor(0.1f, 0.1f, 0.1f, 0.0f);
+
+        int vertex_size = 3;
+        int n_vertices = 3;
+        FloatBuffer vertices = BufferUtils.createFloatBuffer(vertex_size * n_vertices);
+        vertices.put(new float[]{
+                -0.5f, -0.5f, 0.0f,
+                0.5f, -0.5f, 0.0f,
+                0.0f, 0.5f, 0.0f
+        });
+        vertices.flip();
+
+        int vao_handle = glGenVertexArrays();
+        glBindVertexArray(vao_handle);
+
+        int vbo_handle = glGenBuffers();
+        glBindBuffer(GL_ARRAY_BUFFER, vbo_handle);
+        glBufferData(GL_ARRAY_BUFFER, vertices, GL_STATIC_DRAW);
+
+        glVertexAttribPointer(0, vertex_size, GL_FLOAT, false, 3 * 4, 0L);
+        glEnableVertexAttribArray(0);
+
+        glBindBuffer(GL_ARRAY_BUFFER, 0);
+        glBindVertexArray(0);
 
 
-        // Run the rendering loop until the user has attempted to close
-        // the window or has pressed the ESCAPE key.
-        while ( !glfwWindowShouldClose(window) ) {
+        var vertex_shader_source = loadShader("shaders/hello.vert");
+        int vertex_shader = createShader(vertex_shader_source, GL_VERTEX_SHADER);
+
+
+        var fragment_shader_source = loadShader("shaders/hello.frag");
+        int fragment_shader = createShader(fragment_shader_source, GL_FRAGMENT_SHADER);
+
+        int shader_program = createProgram(vertex_shader, fragment_shader);
+
+        glDeleteShader(vertex_shader);
+        glDeleteShader(fragment_shader);
+
+
+        while (!glfwWindowShouldClose(window)) {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // clear the framebuffer
 
-            glfwSwapBuffers(window); // swap the color buffers
+            glUseProgram(shader_program);
+            glBindVertexArray(vao_handle);
+            glDrawArrays(GL_TRIANGLES, 0, 3);
 
-            // Poll for window events. The key callback above will only be
-            // invoked during this call.
+            glfwSwapBuffers(window);
             glfwPollEvents();
         }
+    }
+
+    private int createProgram(int vertex_shader, int fragment_shader) {
+        int shader_program = glCreateProgram();
+        glAttachShader(shader_program, vertex_shader);
+        glAttachShader(shader_program, fragment_shader);
+        glLinkProgram(shader_program);
+
+        int status = glGetProgrami(shader_program, GL_LINK_STATUS);
+        if (status != GL_TRUE){
+            throw new RuntimeException(glGetProgramInfoLog(shader_program));
+        }
+        return shader_program;
+    }
+
+    private int createShader(CharSequence source, int type) {
+        int shader = glCreateShader(type);
+        glShaderSource(shader, source);
+        glCompileShader(shader);
+
+        int status = glGetShaderi(shader, GL_COMPILE_STATUS);
+        if (status != GL_TRUE) {
+            throw new RuntimeException(glGetShaderInfoLog(shader));
+        }
+        return shader;
+    }
+
+    private CharSequence loadShader(String path) {
+        StringBuilder builder = new StringBuilder();
+
+        try (var inputStream = new FileInputStream(path)) {
+            var reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line).append('\n');
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to load shader file!" + System.lineSeparator() + path);
+        }
+
+        return builder.toString();
     }
 
     public static void main(String[] args) {
